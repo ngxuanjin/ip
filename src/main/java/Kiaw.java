2fc92d34f2273eb5d154e75a568ac52a87c2cae4
ngxuanjin <ngxuanjin@gmail.com>
2026-08-25 01:19:57 +0800

    Complete Level-1 Echo

--- src/main/java/Kiaw.java
+++ src/main/java/Kiaw.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Kiaw {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Kiaw.");
         System.out.println("What can I do for you?");
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
